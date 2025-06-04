d8ccdd2d9fe5afe161de374ab21f8de22cffe0a8
Pedro-sugoy <pedrovisckyokoo@gmail.com>
2025-06-04 09:19:03 -0300

    estilização da tela home

--- screens/TelaHome.js
+++ screens/TelaHome.js
@@ -18,25 +18,31 @@ export default function TelaHome() {
         <Text style={styles.title}>GeoAlerta 👨🏻‍💼❗</Text>
 
         <View style={styles.card}>
-          <Text style={styles.subtitle}>
-            Nosso aplicativo foi criado especialmente para pessoas que vivem em áreas vulneráveis a deslizamentos de terra, principalmente quem reside próximas a morros e encostas. Esses deslizamentos são fenômenos naturais causados por diversos fatores, como fortes chuvas, o tipo de solo e relevo da região, além da interferência humana, como desmatamento e construções irregulares em áreas de risco.
+        <Text style={styles.tiulo}>PROPOSTA</Text>
+          <Text style={styles.paragraph}>
+            Nosso aplicativo foi criado especialmente para pessoas que vivem em áreas vulneráveis a deslizamentos de terra, principalmente quem reside próximas a morros e encostas.
           </Text>
-        </View>
 
-        <View style={styles.card}>
-          <Text style={styles.subtitle}>
-            Embora não seja possível evitar completamente os deslizamentos, nosso principal objetivo é monitorar e calcular a probabilidade de riscos nessas regiões, para que os moradores possam receber alertas antecipados e se preparar para situações de perigo. Nosso app vai além dos avisos: oferece orientações práticas e dicas importantes sobre como agir antes, durante e depois de um deslizamento, explicando suas causas e impactos, além de disponibilizar contatos de profissionais e órgãos especializados para análise e suporte.
+          <Text style={styles.tiulo}>CAUSA</Text>
+          <Text style={styles.paragraph}>
+            Esses deslizamentos são fenômenos naturais causados por diversos fatores, como fortes chuvas, o tipo de solo e relevo da região, além da interferência humana, como desmatamento e construções irregulares em áreas de risco.
           </Text>
-        </View>
 
-        <View style={styles.card}>
-          <Text style={styles.subtitle}>
+          <Text style={styles.tiulo}>OBJETIVO</Text>
+          <Text style={styles.paragraph}>
+            Embora não seja possível evitar completamente os deslizamentos, nosso principal objetivo é monitorar e calcular a probabilidade de riscos nessas regiões, para que os moradores possam receber alertas antecipados e se preparar para situações de perigo.
+          </Text>
+
+          <Text style={styles.paragraph}>
+            Nosso app vai além dos avisos: oferece orientações práticas e dicas importantes sobre como agir antes, durante e depois de um deslizamento, explicando suas causas e impactos, além de disponibilizar contatos de profissionais e órgãos especializados para análise e suporte.
+          </Text>
+
+          <Text style={styles.paragraph}>
             Para garantir que os alertas e informações sejam personalizados e precisos, solicitamos que o usuário cadastre o endereço de sua residência. Com essa informação, conseguimos monitorar melhor a área e fornecer um acompanhamento eficaz, ajudando a proteger vidas e minimizar perdas materiais.
           </Text>
-        </View>
 
-        <View style={styles.card}>
-          <Text style={styles.subtitle}>
+          <Text style={styles.tiulo}>PRÓPOSITO</Text>
+          <Text style={styles.paragraph}>
             O GeoAlerta é mais que um simples aplicativo de notificações — é uma ferramenta de prevenção e conscientização que busca apoiar as comunidades em áreas de risco, promovendo segurança e preparo diante de eventos naturais que podem ser devastadores.
           </Text>
         </View>
@@ -50,6 +56,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#dddbd9',
     paddingTop: 20,
     paddingHorizontal: 30,
+    flex: 1,
   },
   scrollContent: {
     alignItems: 'center',
@@ -70,13 +77,14 @@ const styles = StyleSheet.create({
     fontWeight: '700',
     color: '#1e293b',
     marginBottom: 10,
-  },
-  subtitle: {
-    fontSize: 17,
-    color: '#475569',
     textAlign: 'center',
-    lineHeight: 24,
-    maxWidth: 320,
+  },
+  paragraph: {
+    fontSize: 16,
+    color: '#334155',
+    lineHeight: 26,
+    marginBottom: 15,
+    textAlign: 'left',
   },
   card: {
     backgroundColor: '#fff',
@@ -86,7 +94,10 @@ const styles = StyleSheet.create({
     elevation: 4,
     width: '100%',
     maxWidth: 320,
-    borderColor:'green',
-    borderWidth:1
+    borderColor: 'green',
+    borderWidth: 1,
   },
+  tiulo: {
+    fontSize:25
+  }
 });
